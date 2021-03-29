f52b8e3e82ddea5316bf410b73b4aabf46eedf19
Markus <markus.uusitalo@hotmail.fi>
2021-03-29 19:59:48 +0300

    testitiedosto

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import * as csv from 'fast-csv';
 import * as fs from 'fs';
 
+const filename = "test.csv";
+const filepath = __dirname+"/../" + filename;
+
 console.log("start");
 console.time('time');
-fs.createReadStream(__dirname+"/../tweets.csv")
-    .pipe(csv.parse({headers:true, delimiter: ";"}))
+fs.createReadStream(filepath)
+    .pipe(csv.parse({headers:true, delimiter: ";", maxRows: 10}))
     .on('error', error => console.error(error))
     .on('data', row => console.log(row))
     .on('end', (rowCount: number) => console.log(`Parsed ${rowCount} rows`));
