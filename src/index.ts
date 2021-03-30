02a3545ff6b5f89ed76d737d7293ce2aeeda949b
Markus <markus.uusitalo@hotmail.fi>
2021-03-30 20:07:17 +0300

    not working

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,42 @@
 import * as csv from 'fast-csv';
 import * as fs from 'fs';
+import * as cld from 'cld';
 
 const filename = "test.csv";
 const filepath = __dirname+"/../" + filename;
 
-console.log("start");
 console.time('time');
-fs.createReadStream(filepath)
-    .pipe(csv.parse({headers:true, delimiter: ";", maxRows: 10}))
+
+const writeStream = fs.createWriteStream(__dirname + "/../" + "/tmp.csv");
+
+const readStream = fs.createReadStream(filepath)
+
+    // Parse file
+    .pipe(csv.parse({headers:true, strictColumnHandling: true, delimiter: ";"}))
+    .on('data-invalid',(msg) => {
+        readStream.destroy();
+    })
     .on('error', error => console.error(error))
-    .on('data', row => console.log(row))
-    .on('end', (rowCount: number) => console.log(`Parsed ${rowCount} rows`));
+    .on('end', (rowCount: number) => {
+        console.log(`Parsed ${rowCount} rows`);
+    })
+    .on('data', async (data) => {})
+
+    // Transform data
+    .pipe(csv.format({
+        headers: ['id','user','fullname','url','timestamp','replies','likes','retweets','text','language'],
+        delimiter: ";",
+        transform: (row) => {
+            cld.detect(row.text, (err, result) => {
+                row.language = result.languages[0].code;
+                return row;
+            });
+        }
+    }))
+
+    // Write to file
+    .pipe(writeStream);
 
 console.timeEnd('time');
-console.log("end");
+
+
