79599cb12091c56c2ec7a3a1589e1766fc3216bc
Markus <markus.uusitalo@hotmail.fi>
2021-03-30 22:40:53 +0300

    it actually works, insane

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
 import * as csv from 'fast-csv';
 import * as fs from 'fs';
+import {Transform} from 'stream';
 import * as cld from 'cld';
 
 const filename = "test.csv";
 const filepath = __dirname+"/../" + filename;
+
+
+type Rowi = {
+    id: string,
+    user: string,
+    fullname: string,
+    url: string,
+    timestamp: string,
+    replies: string,
+    likes: string,
+    retweets: string,
+    text: string,
+    language: string
+
+}
+
+// class WorkerThing extends Transform {
+//     _transform(chunk, encoding, cb) {
+//         cld.detect(chunk, (err, result) => {
+//             if (result) {
+//                 let output = result.languages[0].code;
+//                 cb(undefined, output);
+//             }
+//             else {
+//                 cb(err);
+//             }
+//         })
+//     }
+// }
+// const workerThing = new WorkerThing();
 
 console.time('time');
 
@@ -23,18 +54,49 @@ const readStream = fs.createReadStream(filepath)
     .on('data', async (data) => {})
 
     // Transform data
-    .pipe(csv.format({
+    .pipe(csv.format<Rowi, Rowi>({
         headers: ['id','user','fullname','url','timestamp','replies','likes','retweets','text','language'],
         delimiter: ";",
     }))
-    .transform((row) => {
+    .transform((row, cb) => {
         cld.detect(row.text, (err, result) => {
-            row.language = result.languages[0].code;
-            return row;
-        })
+            if (result) {
+                let code = result.languages[0].code;
+                cb(null, {
+                    id: row.id,
+                    user: row.user,
+                    fullname: row.fullname,
+                    url: row.url,
+                    timestamp: row.timestamp,
+                    replies: row.replies,
+                    likes: row.likes,
+                    retweets: row.retweets,
+                    text: row.text,
+                    language: code,
+                })
+            }
+        });
+        // setImmediate(() => cb(null, {
+        //     id: row.id,
+        //     user: row.user,
+        //     fullname: row.fullname,
+        //     url: row.url,
+        //     timestamp: row.timestamp,
+        //     replies: row.replies,
+        //     likes: row.likes,
+        //     retweets: row.retweets,
+        //     text: row.text,
+        //     language: "lollero",
+        // }))
+
+        // cld.detect(row.text, (err, result) => {
+        //     row.language = result.languages[0].code;
+        //     return row;
+        // })
     })
 
     // Write to file
+    // .pipe(workerThing)
     .pipe(writeStream);
 
 console.timeEnd('time');
