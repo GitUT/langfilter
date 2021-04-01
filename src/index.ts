ea22f5c19e21f001f6f02c1860512ab35a8bbd7e
Markus <markus.uusitalo@hotmail.fi>
2021-04-01 19:11:25 +0300

    datechecker

--- src/index.ts
+++ src/index.ts
@@ -26,14 +26,15 @@ type outRow = inRow & {
     language: string
 };
 
-let enCount = 0;
-let unknownCount = 0;
+// let enCount = 0;
+// let unknownCount = 0;
 
 const date1 = new Date('2017-11-01 00:00:00.000Z');
 const date2 = new Date('2017-12-16 23:59:59.999Z');
-
 const date3 = new Date('2017-12-17 00:00:00.000Z');
 const date4 = new Date('2018-01-20 23:59:59.999Z');
+const startDate = date1;
+const endDate = date2;
 
 const readStream = fs.createReadStream(inFilePath);
 const writeStream = fs.createWriteStream(outFilePath);
@@ -45,22 +46,24 @@ readStream.pipe(parser)
     .on('error', error => console.log(error))
     .on('end', (rowCount: number) => {
     console.log(`Parsed ${rowCount} rows`);
-    console.log("Detected English: " + enCount);
-    console.log("Unknowns: " + unknownCount);
+    // console.log("Detected English: " + enCount);
+    // console.log("Unknowns: " + unknownCount);
     console.timeEnd('timer');
     })
 
     // Necessary for incrementing rowCount
     .on('data', (row) => {})
 
-    // Format and transform row
+    // Format row
     .pipe(csv.format<outRow, outRow>({
     headers: ['id','user','fullname','url','timestamp','replies','likes','retweets','text'],
     delimiter: ";",
     }))
+
+    // Transform row
     .transform((row) => {
         const date = new Date(row.timestamp.substr(0,19) + ".001Z");
-        if (date > date1 && date < date2) {
+        if (date > startDate && date < endDate) {
             return row;
         } else {
             return null;
